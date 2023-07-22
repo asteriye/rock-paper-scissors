d299708c6ac73870af2b74ef38dcc0c840339b3e
asteriye <bartoszjas8@hotmail.com>
2023-07-22 05:27:18 +0100

    add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice(){
+
+    let computerChoice = Math.floor(Math.random() * 3)
+
+    if(computerChoice === 0) {
+        computerChoice = 'Rock';
+    } else if(computerChoice === 1) {
+        computerChoice = 'Paper';
+    } else {
+        computerChoice = 'Scissors';
+    }
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice())
